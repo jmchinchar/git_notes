bba9b0028e3507d17cc2dd92b1961dadd193f1e8
Kellie Chinchar <kelliechinchar@Kellies-MacBook-Pro.local>
2018-11-06 09:21:52 -0500

    first commit

---
+++ rock.js
@@ -0,0 +1,14 @@
+$('.square1'),click(function(){
+	$('a').html("We've changed the text!");
+	});
+
+$('.square2'),click(function(){
+	$('a').html("We've changed the text!");
+	});
+
+$('.square3'),click(function(){
+	$('a').html("We've changed the text!");
+	});
+
+
+
